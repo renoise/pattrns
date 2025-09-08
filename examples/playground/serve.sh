902b3870af7e06f87bd1b0d8b6394a8f50ebcf0c
Eduard Müller <mail@emuell.net>
2025-09-08 21:17:54 +0200

    update phonic to v0.11
    (cherry picked from commit d44d30b5c0959873468048414b2fa16531da43ab, e4256415f08ac7542e7ccbbaf3ae89f8f6c0666b and c224f3ff700c1fc20c92e859a19d9883e0a72f56)

--- examples/playground/serve.sh
+++ examples/playground/serve.sh
@@ -1,3 +1,14 @@
+#!/bin/bash
+
+set -e -o pipefail
+
+# check if simple-http-server is installed
+if ! command -v simple-http-server &> /dev/null; then
+  echo '*** simple-http-server could not be found. you can install it via:'
+  echo 'cargo binstall simple-http-server'
+  exit 1
+fi
+
 # Note: worker threads (emscripten pthreads) need 
 # "Cross-Origin-Embedder-Policy" HTTP header set to "require-corp"
 # "Cross-Origin-Opener-Policy" HTTP header set to "same-origin"
